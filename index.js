61fad135102d598e0bc8c8c2fabe4d6a1c17a419
manan1234 <794630336@qq.com>
2016-11-14 18:26:39 +0800

    修改音乐

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ $(function(){
 	var sr=3;
 	var br=18;
 	var kaiguan=true;
-	var zi={};
+	var qizi={};
 	var audio=$("audio").get(0);
+	var AI=false;
+	var gamestate="pause";
+	var kongbai={};
 	function l(x){
 		return (x+0.5)*sep+0.5;
 	}
@@ -55,6 +58,58 @@ $(function(){
 		vh--;		
 		$("#htime").html(m+":"+s);
 	}
+// 判断输赢
+	function lj(a,b){
+		return a+"_"+b;
+	}
+	function panduan(x,y,color) {
+		var row=1;
+		var line=1;
+		var zX=1;
+		var yX=1;
+		var i;
+		i=1 ; while ( qizi[lj(x+i,y)] === color ) { row++ ; i++ ; }
+		i=1 ; while ( qizi[lj(x-i,y)] === color ) { row++ ; i++ ; }
+
+		i=1 ; while ( qizi[lj(x,y-i)] === color ) { line++ ; i++ ; }
+		i=1 ; while ( qizi[lj(x,y+i)] === color ) { line++ ; i++ ; }
+
+		i=1 ; while ( qizi[lj(x+i,y+i)] === color ) { zX++ ; i++ ; }
+		i=1 ; while ( qizi[lj(x-i,y-i)] === color ) { zX++ ; i++ ; }
+
+		i=1 ; while ( qizi[lj(x+i,y-i)] === color ) { yX++ ; i++ ; }
+		i=1 ; while ( qizi[lj(x-i,y+i)] === color ) { yX++ ; i++ ; }
+		return Math.max(row,line,zX,yX);
+	}
+	function intel(){
+		var max=-Infinity;
+		var max2=-Infinity;
+		var pos={};
+		var pos2={}
+		for (var k in kongbai) {
+			var x=parseInt(k.split("_")[0]);
+			var y=parseInt(k.split("_")[1]);
+			var q=panduan(x,y,"black");
+			if (q>max) {
+				max=q;
+				pos={x:x,y:y};
+			}
+		}
+		for (var k in kongbai) {
+			var x=parseInt(k.split("_")[0]);
+			var y=parseInt(k.split("_")[1]);
+			var q=panduan(x,y,"white");
+			if (q>max2) {
+				max2=q;
+				pos2={x:x,y:y};
+			}
+		}
+		if (max>max2) {
+			return pos;
+		}else{
+			return pos2;
+		}
+	}
 // 画棋盘
 	function drawqipan(){
 		ctx.save();
@@ -75,10 +130,17 @@ $(function(){
 		circle(11,3);
 		circle(11,11);
 		circle(7,7);
+		for (var i = 0; i <15; i++) {
+			for (var j = 0; j <15; j++) {
+				kongbai[lj(i,j)]=true;
+			}
+		}
 	}
 	drawqipan();
 // 画子
+
 	function drawzi(x,y,color){
+		gamestate="play";
 		ctx.save();
 		ctx.translate(l(x),l(y));
 		ctx.beginPath();
@@ -101,22 +163,73 @@ $(function(){
 			ctx.fillStyle=b;
 		}
 		ctx.fill();
-		ctx.closePath();
+		ctx.closePath();		
 		ctx.restore();
-		zi[x+"_"+y]=color;
+		qizi[x+"_"+y]=color;
+		delete kongbai[lj(x,y)]
+		gamestate="play";
 	}
-// 判断当前点击的是否有落子
-	// function(x,y){
-	// 	return
-	// }
+
+// 生成棋谱
+ function chessManual(){
+ 	ctx.save();
+ 	ctx.font = "20px serif";
+	var i=1;
+	ctx.textAlign="center";
+	ctx.textBaseline="middle";
+	for (var k in qizi) {
+		if (qizi[k]=="black") {
+		ctx.fillStyle="#fff";
+		}else{
+			ctx.fillStyle="black";
+		}
+		var arr=k.split("_");
+		ctx.fillText(i++,l(parseInt(arr[0])),l(parseInt(arr[1])));
+	}	
+	ctx.restore();
+	if ($(".box").find("img").length) {
+		$(".box").find("img").attr("src",canvas.toDataURL());
+	}else{
+		$("<img>").attr("src",canvas.toDataURL()).appendTo(".box");
+	}
+	if ($(".box").find("a").length) {
+		$(".box").find("a").attr("href",canvas.toDataURL());
+	}else{
+		$("<a>").attr("href",canvas.toDataURL()).attr("download","qipu.png").appendTo(".box")
+	}
+	
+ }
 // 落子
 	var t;
-	var t1;
-	$(canvas).on("click",function(e){
+	var t1;	
+	function luozi(e){
 		var x=Math.floor(e.offsetX/sep);
 		var y=Math.floor(e.offsetY/sep);
-		if (zi[x+"_"+y]) {
+		if (qizi[x+"_"+y]) {
 			return;
+		}
+		if (AI) {
+			drawzi(x,y,'black');
+			if (panduan(x,y,"black")>=5) {
+		     	$("#always").css("display","block");
+		     	$("#win").css("display","block");
+		     	$("#yin").css("display","block");
+		     	$("#lose").css("display","block");
+		     	$("#canvas").off("click");
+				$("#ad").get(0).pause();
+
+		     }
+			var p=intel();
+			drawzi(p.x,p.y,"white");
+			if (panduan(p.x,p.y,"white")>=5) {
+		     	$("#always").css("display","block");
+		     	$("#win").css("display","block");
+		     	$("#shu").css("display","block");
+		     	$("#lose").css("display","block");
+		     	$("#canvas").off("click");
+		     	$("#ad").get(0).pause();
+		     };
+			return false;
 		}
 		if (kaiguan) {
 			drawzi(x,y,'black');
@@ -127,8 +240,15 @@ $(function(){
 			 // miaozhen1();
 			 vb=60;
 			 timeb();
-			 clearInterval(t1);
-			
+			 clearInterval(t1);			 
+		     if (panduan(x,y,"black")>=5) {
+		     	$("#always").css("display","block");
+		     	$("#win").css("display","block");
+		     	$("#yin").css("display","block");
+		     	$("#lose").css("display","block");
+		     	$("#canvas").off("click");
+		     	$("#ad").get(0).pause();
+		     }
 			
 		}else{
 			drawzi(x,y,'white');
@@ -140,12 +260,20 @@ $(function(){
 			vh=60;
 			timeh();
 			clearInterval(t);
-			
-			
+			if (panduan(x,y,"white")>=5) {
+		     	$("#always").css("display","block");
+		     	$("#win").css("display","block");
+		     	$("#shu").css("display","block");
+		     	$("#lose").css("display","block");
+		     	$("#canvas").off("click");
+		     	$("#ad").get(0).pause();
+		     };
 		}		
 		kaiguan=!kaiguan;
 		audio.play();
-	})
+		
+	}
+	$(canvas).on("click",luozi)
 // 秒表图
 // var canvas1=$('#white-biao').get(0);
 // var ctx1=canvas1.getContext("2d");
@@ -199,6 +327,7 @@ $(function(){
 // 点击进入页面
  $("#jinru").on("click",function(){
  	$("#dbg").css("display","none");
+ 	$("#ad").get(0).play();
  })
 // 字体变大
 $("#dianji.dj").hover(function(){
@@ -213,17 +342,32 @@ $("#dianji.dj").hover(function(){
 			$("#change").removeClass().addClass("active");	
 
 	})
+	var ts=0;
 	$("#dianji .dj:nth-of-type(2)").on("click",function(){
-		$("#time").css("display","block")	
-		tm=setInterval(time,1000);
+		$("#time").css("display","block")	;
+		if(ts%2==0){
+			tm=setInterval(time,1000);
+			$("#dianji .dj:nth-of-type(2)").html("暂停游戏")
+		}else{
+			clearInterval(tm);
+			$("#dianji .dj:nth-of-type(2)").html("开始游戏")
+
+		}
+		ts++;
 
 	})
 	$("#dianji .dj:nth-of-type(3)").on("click",function(){
-	 	clearInterval(tm);			 
+	 	chessManual();
+	 	$(".box").show();			 
 	})
+	$("#dianji .dj:nth-of-type(4)").on("click",again);
 	$("#change .dj").on("click",function(){
 		$("#xuanguan").css("display","none");
 		$("#change").removeClass().addClass("active1");	
+	})
+	$("#dianji .dj:nth-of-type(5)").on("click",function(){
+	 	$("#dbg").css("display","block");
+	 	$("#ad").get(0).pause();		 
 	})
 //设置页面
 	$("#shezhi").on("click",function(){
@@ -236,4 +380,57 @@ $("#dianji.dj").hover(function(){
 		$("#main").css("display","none");
 		$(".op").css("display","none");
 	})
+// 再来一局
+function again(){
+	$("#always").css("display","none");
+	$("#win").css("display","none");
+	$("#shu").css("display","none");
+	$("#yin").css("display","none");
+	$("#lose").css("display","none");
+	ctx.clearRect(0,0,600,600);
+	drawqipan();
+	kaiguan=true;
+	gamestate="pause";
+	qizi={};
+	$(canvas).off('click').on('click',luozi);
+	$("#ad").get(0).play();
+}
+$("#lose").on("click",again);
+
+//生成棋谱
+$("#scqp").on("click",function(){
+	chessManual();
+	$(".box").show();	
+}) 
+// 点击叉号关闭棋谱
+$("#fork").on("click",function(){
+	$(".box").hide();
+	ctx.clearRect(0,0,600,600);
+	drawqipan();
+	for(var i in qizi){
+		var arr=i.split("_");
+		drawzi(parseInt(arr[0]),parseInt(arr[1]),qizi[i]);
+	}
+})
+// AI
+$("#change .dj:nth-of-type(1)").on("click",function(){
+	if (gamestate==="play") {
+		return;
+	}
+	else{
+		AI=true;
+	}
+	console.log(gamestate);
+})
+$("#change .dj:nth-of-type(2)").on("click",function(){
+	if (gamestate==="play") {
+		return;
+	}
+	else{
+		AI=false;
+	}
+	console.log(gamestate);
+})
+	
+
 })
